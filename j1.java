6858459f2039dddda8fc8fe60674c8eec4fc9299
qrnasr <qrnasr@gmail.com>
2022-02-10 20:53:29 +0300

    Prog22

--- j1.java
+++ j1.java
@@ -4,6 +4,16 @@ class Nasr{
     public static void main(String[] args){
 
     System.out.println("Hello world");
+    int a = 12;
+    int b = 13;
+    System.out.println(a+b);
+
+    if (a>b) {
+        System.out.println("A is greater ! ");
+        
+    } else {
+        System.out.println("B is greater ");
+    }
 
     }
 
