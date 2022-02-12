686ce30355d1c5d12dfa741cac6aa2f2ab2a30b0
qrnasr <qrnasr@gmail.com>
2022-02-12 12:57:11 +0300

    Java2

---
+++ Introduction.java
@@ -0,0 +1,19 @@
+public class Introduction {
+    
+    public static void main(String[] args){
+
+        System.out.println("Hello Kofta");
+        final double pi = 3.14;// use final keyword to have unchanged value
+        final int size = 3;
+        // in c# we use const float = 3.14;
+        System.out.println(pi);
+        System.out.println(size);
+        // variable and methods in java are camel case if the name is made of couple of names
+        // computeArea
+        // Class names, capitaliza the letter of each word, for instance(ComputeArea)
+        // constants: All letters are in capital form and use _ in btween words.
+         
+
+
+}
+}
