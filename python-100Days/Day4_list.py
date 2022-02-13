b6db64fb9d46c0939f72451dab2bf4dc423f5cd5
qrnasr <qrnasr@gmail.com>
2022-02-13 21:51:33 +0300

    go

---
+++ python-100Days/Day4_list.py
@@ -0,0 +1,11 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america.append("Cairo"))
+newList=["Naser","Moan","Malak","Now"]
+print(states_of_america.extend(newList))
+#print(states_of_america.index()
+print(states_of_america.copy())
+print(states_of_america.sort(key=None,reverse=True))
+print(states_of_america.pop())
+print(states_of_america.reverse())
+print(states_of_america.index("Malak"))
