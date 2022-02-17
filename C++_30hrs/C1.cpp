f1530bb69947317b1f55554decc0baf35a8fd6f1
qrnasr <qrnasr@gmail.com>
2022-02-17 19:28:48 +0300

    Introduction

---
+++ C++_30hrs/C1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+// use std:: if using namespace std; is not used
+int main() {
+
+  cout << "Hello World!\n"; // \n to jump to new line
+  // you can use the below if using namespace std; is not called
+ // std::cout << "Hello Budy" std:: <<endl;
+ return 0;
+
+
+}
